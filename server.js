d5767a76b90cb7cb8f1738db96a94a5895da4628
mechandu <mechandu@users.noreply.github.com>
2017-08-30 21:04:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,8 +102,16 @@ var pool=new Pool(config);
              res.status(500).send(err.toString());
          }
          else{
-             res.send(JSON.stringify(result.rows));
-         }
+			if(result.rows.length===0)
+			{
+				res.status(404).send('Article not found');
+			}
+			else
+			{
+			var articleData=result.rows[0];
+			res.send(createTemplate(articleData));
+			}
+		}
      });
  });
 
