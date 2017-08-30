48f6749f046bd8ebe67902d6028515078e900689
mechandu <mechandu@users.noreply.github.com>
2017-08-30 20:08:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').Pool;
+var config={
+    user:'printfchandu',
+    database:'printfchandu',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 var articles={
@@ -83,12 +90,22 @@ function createTemplate(data)
 		 return htmlTemplate;
 }
 
- app.get('/test-db',function(req,res){
-     //make a select request and returna response with the results
- });
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var pool=new Pool(config);
+ app.get('/test-db',function(req,res){
+     //make a select request and returna response with the results
+     pool.query('SELECT * FROM test', function(err,result){
+         if(err){
+             res.status(500).send(err.toString());
+         }
+         else{
+             res.send(JSON.stringify(result));
+         }
+     });
+ });
 
 var counter=0;
 app.get('/counter', function(req, res){
