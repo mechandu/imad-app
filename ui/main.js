7cdd77a7d342594f0f0456bf9f1d582aec32f0c2
mechandu <mechandu@users.noreply.github.com>
2017-08-30 17:57:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,14 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='Chandhu Chandrika';
+var img=document.getElemntById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function()
+{
+    var interval=setInterval(moveRight, 50);
+    img.style.marginLeft='100px';
+};
